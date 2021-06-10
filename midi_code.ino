049d60db9c3bc156da59672b23e92aedad659846
kumarvitthal <77635142+kumarvitthal@users.noreply.github.com>
2021-06-10 16:00:47 +0530

    Update midi_code.ino

--- midi_code.ino
+++ midi_code.ino
@@ -1,3 +1,4 @@
+#include<MIDI.h>
 int rled = 2;
 int gled = 3;
 int bled = 4;
@@ -9,6 +10,7 @@ byte ControllerValue;
 byte ccmidi = 176;
 
 void setup(){
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(rled,OUTPUT);
   pinMode(gled,OUTPUT);
   pinMode(bled,OUTPUT);
@@ -25,19 +27,21 @@ void setup(){
     analogWrite(rled,i);
     delay(1);
   }
+  MIDI_CREATE_DEFAULT_INSTANCE();
+  MIDI.begin(1);
   //start the serial into the boud rate of 115200
   Serial.begin(115200);
 }
 void loop(){
   //wait for a midi message
-  while(Serial.available() <3);
+  while(Serial.available() <3){
   //run the function that will control the leds
-  readmidi();
-  
+    readmidi();
   }
+}
 void readmidi(){
   
-  do{
+  do{    
     //check if there is any message
     if (Serial.available()){
       //store the data into the variables 
@@ -47,13 +51,6 @@ void readmidi(){
       //check if the message is a cc midi message
       if (statusByte==ccmidi){
         digitalWrite(LED_BUILTIN, HIGH);
-        delay(100);
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(100);
-        //Serial monitor wont work when you open the COM port of Arduino in hairless midi
-//         Serial.print(statusByte);
-//         Serial.print(ControllerNumber);
-//         Serial.print(ControllerValue);
         //controll the leds acording to the data bytes
         if(ControllerNumber>=21 && ControllerNumber<=24) analogWrite(rled,ControllerValue*2);
         if(ControllerNumber>=25 && ControllerNumber<=28) analogWrite(gled,ControllerValue*2);
